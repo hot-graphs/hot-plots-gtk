dedc5b450eaaf25343a4d186b15f5a6ed82f7d42
Jan Pipek <jan.pipek@gmail.com>
2017-05-27 18:14:45 +0200

    get_available_points, get_point_meta_data, find_points

--- data_source.py
+++ data_source.py
@@ -1,7 +1,58 @@
 from physt.io import load_json
 from time import time
 import matplotlib.pyplot as plt
+from functools import lru_cache
+import pandas as pd
 
+CSV_FILE = "Adresace_zdroju_s_GPS_vysky_parsed.csv"
+
+@lru_cache(1)
+def get_all_point_metadata(path=CSV_FILE):
+    """Cached reading of the master measure point table.
+    
+    Returns
+    -------
+    pd.DataFrame
+    """
+    data = pd.read_csv(path, sep=";")
+    data["id"] = data["Systém"].str.lower()
+    data.set_index("id", inplace=True)
+    return data
+
+@lru_cache(1)
+def get_available_points():
+    """All available point ids
+    
+    Returns
+    -------
+    ids : list
+    """
+    data = get_all_point_metadata(path=CSV_FILE)
+    return sorted(list(data.index))
+    
+def get_point_meta_data(id):
+    """Meta-data for one specific measure point.
+    
+    Returns
+    -------
+    data : pd.Series
+    """
+    try:
+        return get_all_point_metadata(path=CSV_FILE).loc[id]
+    except KeyError:
+        raise RuntimeError("Point {0} not in the database.".format(id))
+    
+@lru_cache(20)
+def find_points(address):
+    """All points at an address
+    
+    Returns
+    -------
+    points : pd.DataFrame
+        Empty or non-empty
+    """
+    data = get_all_point_metadata(path=CSV_FILE)
+    return data[data["Adresa"] == address]
 
 def read_data(path="data/Vinohrady.json"):
     return load_json(path)
