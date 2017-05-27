4271b4b2447ac475941fad6444dbc96b98a0bad1
Jan Pipek <jan.pipek@gmail.com>
2017-05-27 10:22:18 +0200

    plot to file

--- data_source.py
+++ data_source.py
@@ -1,12 +1,24 @@
 from physt.io import load_json
-from functools import lru_cache
+from time import time
+import matplotlib.pyplot as plt
 
 
-@lru_cache(1)
 def read_data(path="data/Vinohrady.json"):
     return load_json(path)
 
-def plot(ax, x="hodina", y="teplota"):
-    hist = read_data()
+def plot_to_axis(source, ax, x="hodina", y="teplota"):
+    hist = read_data(source)
     projection = hist.projection(x, y)
-    projection.plot(ax=ax)
+    projection.plot(ax = ax)
+    
+def plot_to_file(source, path="output.png", x="hodina", y="teplota", dpi=300):
+    hist = read_data(source)
+    projection = hist.projection(x, y)
+    fig, ax = plt.subplots()
+    t = time()
+    projection.plot("image", ax=ax)
+    # print("plotting", time() - t)
+    t = time()
+    fig.tight_layout()
+    fig.savefig(path, dpi=dpi)
+    # print("saving", time() - t)
