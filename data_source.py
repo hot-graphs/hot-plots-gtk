4615cbe569a842d0a6b54417fab0ecc14f5783df
Jan Pipek <jan.pipek@gmail.com>
2017-05-27 10:55:32 +0200

    plot_to_file accepts resolution

--- data_source.py
+++ data_source.py
@@ -11,14 +11,14 @@ def plot_to_axis(source, ax, x="hodina", y="teplota"):
     projection = hist.projection(x, y)
     projection.plot(ax = ax)
     
-def plot_to_file(source, path="output.png", x="hodina", y="teplota", dpi=300):
+def plot_to_file(source, path="output.png", x="hodina", y="teplota", xsize=1024, ysize=800):
     hist = read_data(source)
     projection = hist.projection(x, y)
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, ysize/xsize * 10))
     t = time()
     projection.plot("image", ax=ax)
     # print("plotting", time() - t)
     t = time()
     fig.tight_layout()
-    fig.savefig(path, dpi=dpi)
+    fig.savefig(path, dpi=xsize/10)
     # print("saving", time() - t)
