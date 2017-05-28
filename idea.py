270613c28737d6c99c2dbd58ebe45504f5c00fbb
Marcel Plch <gmarcel.plch@gmail.com>
2017-05-28 10:38:58 +0200

    Slider, yay!

--- idea.py
+++ idea.py
@@ -75,10 +75,18 @@ class IdeaWin(Gtk.Window):
         self.opt_box = Gtk.VBox()
         self.graph_box.pack_end(self.opt_box, False, False, 0)
 
-        self.listbox =Gtk.ListBox()
+        self.listbox = Gtk.ListBox()
         self.listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        self.opt_box.pack_start(self.listbox, False, False, 0)
 
-
+        row = Gtk.ListBoxRow()
+        self.listbox.add(row)
+        vbox = Gtk.VBox()
+        row.add(vbox)
+        hbox = Gtk.Box()
+        vbox.pack_start(hbox, False, False, 0)
+        label = Gtk.Label("X axis: ")
+        hbox.pack_start(label, False, False, 0)
 
         interval_store = Gtk.ListStore(str)
         intervals = ["hour", "month"]
@@ -92,8 +100,33 @@ class IdeaWin(Gtk.Window):
         self.renderer_text = Gtk.CellRendererText()
         self.interval_combo.pack_start(self.renderer_text, False)
         self.interval_combo.add_attribute(self.renderer_text, "text", 0)
+        hbox.pack_start(self.interval_combo, False, False, 0)
 
-        self.opt_box.pack_start(self.interval_combo, False, False, 0)
+
+        row = Gtk.ListBoxRow()
+        self.listbox.add(row)
+        vbox = Gtk.VBox()
+        hbox = Gtk.Box()
+        row.add(vbox)
+        vbox.pack_start(hbox, False, False, 0)
+        check = Gtk.CheckButton("Greenary Filter")
+        hbox.pack_start(check, False, False, 0)
+        inner_vbox = Gtk.VBox()
+        vbox.pack_start(inner_vbox, False, False, 0)
+        slider_box = Gtk.Box()
+        inner_vbox.pack_start(slider_box, False, False, 50)
+
+        ad1 = Gtk.Adjustment(0, 0, 100, 5, 10, 0)
+        ad2 = Gtk.Adjustment(0, 0, 100, 5, 10, 0)
+        ad3 = Gtk.Adjustment(0, 0, 100, 5, 10, 0)
+        
+        min_scale = Gtk.Scale(
+            orientation=Gtk.Orientation.HORIZONTAL, adjustment=ad1)
+        min_scale.set_digits(0)
+        min_scale.set_hexpand(True)
+        min_scale.connect("value-changed", self.scale_moved)
+
+        slider_box.pack_start(min_scale, True, True, 0)
 
         self.scrolledwindow = Gtk.ScrolledWindow()
         self.scrolledwindow.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.NEVER)
@@ -105,7 +138,7 @@ class IdeaWin(Gtk.Window):
 
     def _plot(self):
         data = get_temperature_data(id=self.gr_id, axes=(self.x, self.y))
-        plot_temperature_data(data, path="output.svg")
+        plot_temperature_data(data, path="output.svg", width= 600, height=400)
         child = self.scrolledwindow.get_child()
         if child:
             self.scrolledwindow.remove(child)
@@ -120,14 +153,17 @@ class IdeaWin(Gtk.Window):
         )
         self.map_controller.send_command(cmd='start')
 
+    def on_button_toggled(self):
+        pass
+
+    def scale_moved(self, widget):
+        pass
+
     def clean_up(self, *args):
         if self.map_controller:
             self.map_controller.send_command_if_open(cmd='stop')
 
         Gtk.main_quit()
-
-    def on_button_toggled(self, widget, name):
-        pass
 
     def on_interval_combo_changed(self, combo):
         tree_iter = combo.get_active_iter()
