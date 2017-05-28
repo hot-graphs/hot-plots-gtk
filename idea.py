e82a6195126a3ab3d3abdeacc919866d63a149e8
Marcel Plch <gmarcel.plch@gmail.com>
2017-05-28 09:19:58 +0200

    better composition

--- idea.py
+++ idea.py
@@ -19,45 +19,32 @@ class IdeaWin(Gtk.Window):
         self.y = "temperature"
         self.map_controller = None
         self.connect("delete-event", self.clean_up)
-        self.set_default_size(1280, 720)
-        self.grid = Gtk.Grid()
-        self.add(self.grid)
+        self.outerbox = Gtk.VBox()
+        self.add(self.outerbox)
+        self.outerbox.set_hexpand(True)
+        self.outerbox.set_vexpand(True)
+        self.toolbox = Gtk.Box()
+        self.outerbox.pack_start(self.toolbox, False, False, 0)
 
         button1 = Gtk.RadioButton.new_from_widget(None)
         button1.set_label("Single sensor")
         button1.connect("toggled", self.on_button_toggled, "1")
-        self.grid.attach(button1, 0, 0, 1, 1)
+        self.toolbox.pack_start(button1, False, False, 0)
 
         button2 = Gtk.RadioButton.new_from_widget(button1)
         button2.set_label("Comparison")
         button2.connect("toggled", self.on_button_toggled, "2")
-        self.grid.attach(button2, 1, 0, 1, 1)
+        self.toolbox.pack_start(button2, False, False, 0)
 
         self.map_button = Gtk.Button(label="Open Map")
         self.map_button.connect("clicked", self.on_map_button_clicked)
-        self.grid.attach(self.map_button, 3, 0, 1, 1)
+        self.toolbox.pack_end(self.map_button, False, False, 0)
 
-        self.scrolledwindow_opts = Gtk.ScrolledWindow()
-        self.scrolledwindow_opts.set_hexpand(True)
-        self.scrolledwindow_opts.set_vexpand(True)
+        self.mainbox = Gtk.Box()
+        self.outerbox.pack_end(self.mainbox, True, True, 0)
 
-        interval_store = Gtk.ListStore(str)
-        intervals = ["hour", "month"]
-        for interval in intervals:
-            interval_store.append([interval])
-
-        self.interval_combo = Gtk.ComboBox.new_with_model(interval_store)
-        self.interval_combo.set_active(0)
-
-        self.interval_combo.connect("changed", self.on_interval_combo_changed)
-        self.renderer_text = Gtk.CellRendererText()
-        self.interval_combo.pack_start(self.renderer_text, True)
-        self.interval_combo.add_attribute(self.renderer_text, "text", 0)
-
-        self.scrolledwindow = Gtk.ScrolledWindow()
-        self.scrolledwindow.set_hexpand(True)
-        self.scrolledwindow.set_vexpand(True)
-        self.grid.attach(self.scrolledwindow, 1, 1, 5, 1)
+        self.filebox = Gtk.Box()
+        self.mainbox.pack_end(self.filebox, False, True, 0)
 
         self.filesystemTreeStore = Gtk.TreeStore(str, str)
         parents = {}
@@ -75,13 +62,55 @@ class IdeaWin(Gtk.Window):
         self.filesystemTreeView.append_column(filesystemColumn)
 
         self.scrolledwindow_tree = Gtk.ScrolledWindow()
-        self.scrolledwindow_tree.set_hexpand(True)
-        self.scrolledwindow_tree.set_vexpand(True)
-
-        self.grid.attach(self.scrolledwindow_tree, 7, 1, 1, 1)
+        self.scrolledwindow_tree.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.ALWAYS)
+        self.outerbox.set_hexpand(False)
+        self.outerbox.set_vexpand(True)
         self.scrolledwindow_tree.add(self.filesystemTreeView)
         self.filesystemTreeView.connect("row-activated", self.on_click)
+        self.filebox.pack_start(self.scrolledwindow_tree, True, True, 0)
 
+        self.graph_box = Gtk.VBox()
+        self.mainbox.pack_start(self.graph_box, False, False, 0)
+
+        self.opt_box = Gtk.VBox()
+        self.graph_box.pack_end(self.opt_box, False, False, 0)
+
+        self.listbox =Gtk.ListBox()
+        self.listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+
+
+
+        interval_store = Gtk.ListStore(str)
+        intervals = ["hour", "month"]
+        for interval in intervals:
+            interval_store.append([interval])
+
+        self.interval_combo = Gtk.ComboBox.new_with_model(interval_store)
+        self.interval_combo.set_active(0)
+
+        self.interval_combo.connect("changed", self.on_interval_combo_changed)
+        self.renderer_text = Gtk.CellRendererText()
+        self.interval_combo.pack_start(self.renderer_text, False)
+        self.interval_combo.add_attribute(self.renderer_text, "text", 0)
+
+        self.opt_box.pack_start(self.interval_combo, False, False, 0)
+
+        self.scrolledwindow = Gtk.ScrolledWindow()
+        self.scrolledwindow.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.NEVER)
+        self.graph_box.pack_start(self.scrolledwindow, True, True, 0)
+        self.gr_id = self.filesystemTreeStore.get(self.filesystemTreeStore.get_iter((5,1)), 1)[0]
+        self._plot()
+
+        self.show_all()
+
+    def _plot(self):
+        data = get_temperature_data(id=self.gr_id, axes=(self.x, self.y))
+        plot_temperature_data(data, path="output.svg")
+        child = self.scrolledwindow.get_child()
+        if child:
+            self.scrolledwindow.remove(child)
+        self.img = Gtk.Image.new_from_file('output.svg')
+        self.scrolledwindow.add(self.img)
         self.show_all()
 
     def on_map_button_clicked(self, widget):
@@ -107,37 +136,12 @@ class IdeaWin(Gtk.Window):
             interval = model[tree_iter][0]
             print("Selected: interval=%s of type %s" % (interval, type(interval)))
             self.x = interval
-            self._replot()
+            self._plot()
 
     def on_click(self, widget, coords, wobble):
-        gr_id = self.filesystemTreeStore.get(self.filesystemTreeStore.get_iter(coords), 1)[0]
+        self.gr_id = self.filesystemTreeStore.get(self.filesystemTreeStore.get_iter(coords), 1)[0]
         # TODO: replot...
-        data = get_temperature_data(id=gr_id, axes=("hour", "temperature"))
-        plot_temperature_data(data, path="output.svg")
-        child = self.scrolledwindow.get_child()
-        if child:
-            self.scrolledwindow.remove(child)
-        self.img = Gtk.Image.new_from_file('output.svg')
-        self.scrolledwindow.add(self.img)
-        self.show_all()
-        """hist = read_data(self.path)
-        projection = hist.projection(self.x, self.y)
-        fig, ax = plt.subplots()
-        t = time()
-        projection.plot("image", ax=ax)
-        # print("plotting", time() - t)
-        t = time()
-        fig.tight_layout()
-        fig.savefig(path, dpi=dpi)
-        # print("saving", time() - t)"""
-
-        """child = self.scrolledwindow.get_child()
-        if child:
-            self.scrolledwindow.remove(child)
-        self._plot_to_file()
-        self.img = Gtk.Image.new_from_file('output.svg')
-        self.scrolledwindow.add(self.img)
-        self.show_all()"""
+        self._plot()
 
 def main():
     win = IdeaWin()
